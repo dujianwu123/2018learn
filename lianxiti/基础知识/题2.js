3b2414dd97e39eff592bc8e948f94a2c0a400f99
dujianwu123 <355504677@qq.com>
2018-11-09 10:23:19 +0800

    d

--- lianxiti/基础知识/题2.js
+++ lianxiti/基础知识/题2.js
@@ -1,10 +1,10 @@
 if('m' in window){
-  var m = 1 && 12;
+    var m = 1 && 12
 }
-console.log(m);
+console.log(m)
 
-let n = 10;
+let n = 10
 if (!('n' in window)){
-  let n = n + 30; //报错，因为let 不预解释，正常是先准备值，在给n赋值，但是在n+30 的时候由于是let不会像var那样预解释，所以在此时n是不存在的
+    let n = n + 30 //报错，因为let 不预解释，正常是先准备值，在给n赋值，但是在n+30 的时候由于是let不会像var那样预解释，所以在此时n是不存在的
 }
-console.log(n);
+console.log(n)
