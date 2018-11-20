1149d93b13bda0f0c4e81c56b3849ca13df3c493
dujianwu123 <dujianwu123@126.com>
2018-11-21 00:21:09 +0800

    xx

--- lianxiti/实例/瀑布流/js/index.js
+++ lianxiti/实例/瀑布流/js/index.js
@@ -1,6 +1,7 @@
 $(function () {
   let page = 0;
   let imgData = null;
+  let isRun = false;
   //获取数据
   let queryData = () => {
     page++;
@@ -17,7 +18,7 @@ $(function () {
 
 
   queryData();
-  console.log(imgData);
+  // console.log(imgData);
 
   //数据绑定
   let queryHTML = item => {
@@ -30,25 +31,56 @@ $(function () {
             <span>${title}</span>
             </a>`;
   };
-  let $boxList = $('.box > li');
-  let boxList = [].slice.call($boxList);
-  for (let i = 0; i < imgData.length; i += 3) {
-     let item1 = imgData[i];
-     let item2 = imgData[i+1];
-     let item3 = imgData[i+2];
+  let bindHTML = () => {
+      let $boxList = $('.box > li');
+      let boxList = [].slice.call($boxList);
+      for (let i = 0; i < imgData.length; i += 3) {
+        let item1 = imgData[i];
+        let item2 = imgData[i+1];
+        let item3 = imgData[i+2];
 
-     boxList.sort(function(a,b){
-      return a.offsetHeight - b.offsetHeight;
-     });
-     console.log(boxList);
-     if(item1){
-      boxList[0].innerHTML += queryHTML(item1);
-     }
-     if(item2){
-      boxList[1].innerHTML += queryHTML(item2);
-     }
-     if(item3){
-      boxList[2].innerHTML += queryHTML(item3);
-     }
+        boxList.sort(function(a,b){
+          return a.offsetHeight - b.offsetHeight;
+        });
+        // console.log(boxList);
+        if(item1){
+          boxList[0].innerHTML += queryHTML(item1);
+        }
+        if(item2){
+          boxList[1].innerHTML += queryHTML(item2);
+        }
+        if(item3){
+          boxList[2].innerHTML += queryHTML(item3);
+        }
+      }
+
+      isRun = false;
+
   }
+  bindHTML();
+
+
+  //滚动到底部加载更多数据
+  
+  $(window).on('scroll',()=>{
+    
+    let winH = $(window).outerHeight();
+    let pageH = document.documentElement.scrollHeight || document.body.scrollHeight;
+    let srcT = $(window).scrollTop();
+    
+    //卷去的高度，大于 真实高度-一屏幕高度
+    if((srcT+100) >= (pageH - winH)){
+      if(isRun) return;
+      isRun = true;
+
+      if(page > 5){
+          alert('没有数据了');
+          return ;
+      }
+
+      queryData();
+      bindHTML();
+    }
+  });
+
 });
