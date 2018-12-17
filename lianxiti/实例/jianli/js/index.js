fbb8cbdf175c08790cd7117fed4c8e8233b951bf
dujianwu123 <dujianwu123@126.com>
2018-12-17 09:21:56 +0800

    xx

--- lianxiti/实例/jianli/js/index.js
+++ lianxiti/实例/jianli/js/index.js
@@ -30,17 +30,17 @@ let loadingRender = (function () {
   let maxDelay = function () {
     delayTimer = setTimeout((callback) => {
       if (n / len >= 0.9) {
-        $current.css('width','100%');//当网慢加载到90%时，瞬间到100%，让用户以为到100%
+        $current.css('width', '100%');//当网慢加载到90%时，瞬间到100%，让用户以为到100%
         callback && callback();
         return;
       }
       alert('非常遗憾，当前您的网络状况不加，请稍后在试!');
       //此时我们不应该继续加载图片，而是让其关掉页面或者是跳转到其它页面
-      window.location.href='http://www.baidu.com';
+      window.location.href = 'http://www.baidu.com';
     }, 10000);
   };
   //DONE：完成
-  let done = function done(){
+  let done = function done() {
     //停留一秒在移除进入下一环节
     let timer = setTimeout(() => {
       $loadingBox.remove();
@@ -54,4 +54,94 @@ let loadingRender = (function () {
   }
 })();
 
-loadingRender.init();
+// loadingRender.init();
+
+let phoneRender = (function () {
+  let $phoneBox = $('.phoneBox');
+  let $time = $phoneBox.find('h2>span');
+  let $answer = $phoneBox.find('.answer');
+  let $answerMarkLink = $answer.find('.markLink');
+  let $hang = $phoneBox.find('.hang');
+  let $hangMarkLink = $hang.find('.markLink');
+  let answerBell = $('#answerBell').get(0);
+  let introduction = $('#introduction').get(0);
+  let answerMarkTouch = function () {
+    //1、remove answer
+    $answer.remove();
+    answerBell.pause();
+    $(answerBell).remove();//必须先暂停在移除，否则有时浏览器还会播放声音
+
+    //2、hang显示出来
+    $hang.css('transform', 'translateY(0rem)');
+    $time.css('display', 'block');
+    introduction.play();
+    computedTime();
+  };
+
+  //->计算播放时间
+  let autoTimer = null;
+  let computedTime = function computedTime() {
+    // let duration = 0;
+    //->我们让AUDIO播放，首先会去加载资源，部分资源加载完成才会播放，才会计算出总时间
+    //所以把获取信息放在canplay事件中
+    // introduction.oncanplay = function () {
+    //   duration = introduction.duration;
+    // }
+    autoTimer = setInterval(() => {
+      let val = introduction.currentTime;//当前播放的时间 单位是秒
+      let duration = introduction.duration;
+      if (val >= duration) {
+        clearInterval(autoTimer);
+        closePhone();
+        return;
+      }
+      let minute = Math.floor(val / 60);
+      let second = Math.floor(val - minute * 60);
+      minute = minute < 10 ? '0' + minute : minute;
+      second = second < 10 ? '0' + second : second;
+      $time.html(`${minute}:${second}`);
+    }, 1000);
+  };
+
+  //->关闭PHONE
+  let closePhone = function () {
+    clearInterval(autoTimer);
+    introduction.pause();
+    $(introduction).remove();
+    $phoneBox.remove();
+  }
+
+  return {
+    init: function () {
+      //=>播放bell
+      answerBell.play();
+      answerBell.volume = 0.3;
+
+      $answerMarkLink.on('click', answerMarkTouch);
+      $hangMarkLink.on('click', closePhone);
+    }
+  }
+})();
+/**
+ * 在开发中，由于当前项目板块众多（每一个板块都是一个单例），我们最好规划一种机制，通过标识的判断可以
+ * 让程序只执行对应板块的内容，这样开发哪个版块，我们就把标识改为啥（HASH路由控制）
+ * 
+ */
+
+let url = window.location.href;//获取当前页面的URL地址，location.href=xxx这种写法是让其跳转跳转到某一个页面
+
+let well = url.indexOf('#');
+let hash = well === -1 ? null : url.substr(well + 1);
+console.log('hash', hash);
+switch (hash) {
+  case 'loading':
+    loadingRender.init();
+    break;
+  case 'phone':
+    phoneRender.init();
+    break;
+
+  default:
+    break;
+}
+
